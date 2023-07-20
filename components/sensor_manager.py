1b93739fcbbc175ed3485bca20d3b5beae114851
Aude <47717187+audevuilli@users.noreply.github.com>
2023-07-20 18:10:59 +0100

    update

--- components/sensor_manager.py
+++ components/sensor_manager.py
@@ -14,6 +14,7 @@ import datetime
 import serial
 import time
 import asyncio
+from typing import List
 
 from data import Sensor, SensorValue
 from components.types import SensorReader
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import datetime
 import components
 
 HWID = 'SCALE'
