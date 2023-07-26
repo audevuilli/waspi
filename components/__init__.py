4f5f03ddf95e0493e6d7139c93360938e7f8f07d
Aude <47717187+audevuilli@users.noreply.github.com>
2023-07-26 16:30:07 +0100

    move pySerialTransfer

--- components/__init__.py
+++ components/__init__.py
@@ -2,10 +2,11 @@
 from components.sensor_manager import SensorInfo, SensorReporter, SerialReceiver
 from components.messengers import MQTTMessenger
 from components.waspi_util import *
+from components.pySerialTransfer import pySerialTransfer
 
 __all__ = [
     SensorInfo,
     SensorReporter, 
     SerialReceiver, 
-    MQTTMessenger
+    MQTTMessenger,
 ]
