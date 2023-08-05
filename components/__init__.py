b549067ddf64783de4da28ba099f06c6b8082446
Aude <47717187+audevuilli@users.noreply.github.com>
2023-08-05 15:10:32 +0100

    update __init__.py components

--- components/__init__.py
+++ components/__init__.py
@@ -1,6 +1,6 @@
 """Waspi components."""
 #from components.sensor_manager import SensorReporter, SerialReceiver
-from components.old_sensor_manager import SensorReporter, SerialReceiver
+from components.sensor_manager import SensorReporter, SerialReceiver
 from components.message_factories import MessageBuilder
 from components.messengers import MQTTMessenger
 from components.waspi_util import *
