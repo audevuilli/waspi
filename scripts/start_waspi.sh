ffcfd8ba18a4abb0afb2073ff8405a3a4deff876
Aude <47717187+audevuilli@users.noreply.github.com>
2023-08-07 10:01:17 +0100

    create script start_waspi.sh

---
+++ scripts/start_waspi.sh
@@ -0,0 +1,7 @@
+#!bin/bash
+
+sudo ln -sf $HOME/waspi/services/run_waspi.service /usr/lib/systemd/system
+sudo systemctl daemon-reload
+sudo systemctl enable run_waspi.service
+sudo systemctl start run_waspi.service
+systemctl status run_waspi.service
