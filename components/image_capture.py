6d1535a1e63a44b782e33683a7c677003bf3cda5
audevuilli <aude.vuilli@gmail.com>
2023-08-08 14:48:31 +0100

    add image test1

--- components/image_capture.py
+++ components/image_capture.py
@@ -4,7 +4,6 @@ import time
 picam2 = Picamera2() 
 camera_config = picam2.create_preview_configuration() 
 picam2.configure(camera_config) 
-picam2.start_preview(Preview.QTGL) 
 picam2.start() 
 time.sleep(2) 
 picam2.capture_file("test.jpg")
