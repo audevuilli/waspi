24555f95aa775c759c0d4070bbb578250477f886
Aude <47717187+audevuilli@users.noreply.github.com>
2023-08-23 15:19:32 +0100

    update run_waspi.service

--- components/__init__.py
+++ components/__init__.py
@@ -1,13 +1,12 @@
 """Waspi components."""
-#from components.sensor_manager import SensorReporter, SerialReceiver
-from components.accel_logger.py import AccelLogger
+#from components.accel_logger.py import AccelLogger
 from components.sensor_manager import SensorReporter, SerialReceiver
 from components.message_factories import MessageBuilder
 from components.messengers import MQTTMessenger
 from components.waspi_util import *
 
 __all__ = [
-    AccelLogger, 
+    #AccelLogger, 
     SensorReporter, 
     SerialReceiver, 
     MessageBuilder,
