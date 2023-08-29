f1fea57a767075b7d030831eeef485d0b9246610
aude <aude.vuilli@gmail.com>
2023-08-29 14:03:05 +0100

    add script setup_heartbeat

---
+++ scripts/setup_heartbeat.sh
@@ -0,0 +1,7 @@
+#!bin/bash
+
+sudo ln -sf $HOME/waspi/services/heartbeat.service /usr/lib/systemd/system
+sudo systemctl daemon-reload
+sudo systemctl enable heartbeat.service
+sudo systemctl start heartbeat.service
+systemctl status heartbeat.service
