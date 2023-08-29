43dbcdddb8ac4139cdb45106ee0ed3bb46bb0693
Aude <47717187+audevuilli@users.noreply.github.com>
2023-08-29 16:55:11 +0100

    update accel_logger.py

--- components/accel_logger.py
+++ components/accel_logger.py
@@ -10,21 +10,9 @@ import spidev
 import wave
 import array
 
+from components import data
+from components.waspi_types import Recording
 
-# Define the SPI Communication Bus 
-spi_channel = 0
-spi_max_speed_hz = 1200000
-vref = 3.3
-
-# ADC (channel and bit depth) define recording sampling rate (in HZ)
-adc_bitdepth = 10
-sampling_rate = 20000
-sampling_duration = 30  #30 seconds
-sampling_number = sampling_duration * sampling_rate
-#sampling_interval = 180  #30 minutes in sec. 
-
-ADC_CHANNEL_0 = 0
-ADC_CHANNEL_1 = 1
 
 class AccelLogger():
     """ Sample data from the accelerometer. 
@@ -115,49 +103,3 @@ class AccelLogger():
 
         # Close the SPI Interface
         spi.close()
-
-
-SPI_CHANNEL = 0
-SPI_MAX_SPEED_HZ = 1200000
-VREF = 3.3
-
-ADC_CHANNEL_0 = 0
-ADC_CHANNEL_1 = 1
-ADC_BITDEPTH = 10
-ACCEL_SAMPLERATE = 20000 #16KHz
-ACCEL_SAMPLEDURATION = 30 #30 seconds 
-
-
-def main():
-
-    """Create the Accelerometer_Logger object."""
-    accel0_logger = AccelLogger(
-        spi_channel = SPI_CHANNEL,
-        spi_max_speed_hz = SPI_MAX_SPEED_HZ,
-        vref = VREF,
-        adc_channel = ADC_CHANNEL_0, 
-        adc_bitdepth = ADC_BITDEPTH, 
-        sampling_rate = ACCEL_SAMPLERATE, 
-        sampling_duration = ACCEL_SAMPLEDURATION
-        )
-    
-    accel1_logger = AccelLogger(
-        spi_channel = SPI_CHANNEL,
-        spi_max_speed_hz = SPI_MAX_SPEED_HZ,
-        vref = VREF,
-        adc_channel = ADC_CHANNEL_0, 
-        adc_bitdepth = ADC_BITDEPTH, 
-        sampling_rate = ACCEL_SAMPLERATE, 
-        sampling_duration = ACCEL_SAMPLEDURATION
-        )
-
-    print(f"Start Recording Accel 0")
-    record_accel0 = accel0_logger.record_file()
-    print(f"End Recording Accel 0: {record_accel0}")
-    print("")
-    print(f"Start Recording Accel 1")
-    record_accel1 = accel1_logger.record_file()
-    print(f"End Recording Accel 1: {record_accel1}")
-
-if __name__ == '__main__':
-    main()
---
+++ components/new_main.py
@@ -0,0 +1,43 @@
+import datetime
+import time
+
+from components.accel_logger import AccelLogger
+
+SPI_CHANNEL = 0
+SPI_MAX_SPEED_HZ = 1200000
+VREF = 3.3
+
+ADC_CHANNEL_0 = 0
+ADC_CHANNEL_1 = 1
+ADC_BITDEPTH = 10
+ACCEL_SAMPLERATE = 20000 #16KHz
+ACCEL_SAMPLEDURATION = 30 #30 seconds 
+
+"""Create the Accelerometer objects."""
+accel0_logger = AccelLogger(
+    spi_channel = SPI_CHANNEL,
+    spi_max_speed_hz = SPI_MAX_SPEED_HZ,
+    vref = VREF,
+    adc_channel = ADC_CHANNEL_0, 
+    adc_bitdepth = ADC_BITDEPTH, 
+    sampling_rate = ACCEL_SAMPLERATE, 
+    sampling_duration = ACCEL_SAMPLEDURATION
+    )
+
+accel1_logger = AccelLogger(
+    spi_channel = SPI_CHANNEL,
+    spi_max_speed_hz = SPI_MAX_SPEED_HZ,
+    vref = VREF,
+    adc_channel = ADC_CHANNEL_0, 
+    adc_bitdepth = ADC_BITDEPTH, 
+    sampling_rate = ACCEL_SAMPLERATE, 
+    sampling_duration = ACCEL_SAMPLEDURATION
+    )
+
+print(f"Start Recording Accel 0")
+record_accel0 = accel0_logger.record_file()
+print(f"End Recording Accel 0: {record_accel0}")
+print("")
+print(f"Start Recording Accel 1")
+record_accel1 = accel1_logger.record_file()
+print(f"End Recording Accel 1: {record_accel1}")
