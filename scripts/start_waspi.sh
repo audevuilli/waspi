e713d9332da0f2656cf87ee36f3be9021b530a6b
Aude <47717187+audevuilli@users.noreply.github.com>
2023-08-31 19:28:57 +0100

    update scripts and services

--- scripts/start_waspi.sh
+++ scripts/start_waspi.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 
-sudo ln -sf $HOME/waspi/src/waspi/services/run_waspi.service /usr/lib/systemd/system
+sudo ln -sf $HOME/waspi/services/run_waspi.service /usr/lib/systemd/system
 sudo systemctl daemon-reload
 sudo systemctl enable run_waspi.service
 sudo systemctl start run_waspi.service
