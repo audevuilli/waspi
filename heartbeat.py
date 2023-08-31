72b72427cf466c3bde7396c96dee50a4813de49f
audevuilli <aude.vuilli@gmail.com>
2023-08-31 13:53:33 +0100

    add test_image.py script

--- heartbeat.py
+++ heartbeat.py
@@ -5,6 +5,7 @@ import logging
 
 import config_mqtt
 
+MQTT_TOPIC = 'UCL/OPS/Garden/wasps_4/heartbeat/'
 
 # Setup the main logger
 logging.basicConfig(
@@ -18,10 +19,11 @@ starttime=time.time()
 
 while True:
   payload = "" + datetime.datetime.now().strftime("%Y-%m-%d") + "T" + datetime.datetime.now().strftime("%X")
-  sleeper = 360.0 - ((time.time() - starttime) % 360.0)
+  #sleeper = 360.0 - ((time.time() - starttime) % 360.0)
+  sleeper = 10
   time.sleep(sleeper)
   try:
     mqtt_auth = { 'username': config_mqtt.DEFAULT_MQTTCLIENT_USER, 'password': config_mqtt.DEFAULT_MQTTCLIENT_PASS }
-    publish.single(config_mqtt.DEFAULT_TOPIC, payload, hostname=config_mqtt.DEFAULT_HOST, port=config_mqtt.DEFAULT_PORT, auth=mqtt_auth)
+    publish.single(MQTT_TOPIC, payload, hostname=config_mqtt.DEFAULT_HOST, port=config_mqtt.DEFAULT_PORT, auth=mqtt_auth)
   except:
     logging.info("No connection")
