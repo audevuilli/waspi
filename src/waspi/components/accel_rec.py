d4562ec8d39bdad7fcd49633ec783476fc21fdce
audevuilli <aude.vuilliomenet.18@ucl.ac.uk>
2025-05-23 16:13:24 +0100

    formatting

--- src/waspi/components/accel_rec.py
+++ src/waspi/components/accel_rec.py
@@ -8,25 +8,21 @@ from waspi.components.types import AudioRecorder
 
 class AccelRecorder(AudioRecorder):
     """An AccelRecorder that records a 2-channel audio file."""
-    
+
     duration: float
     """The duration of the audio file in seconds."""
-    
+
     samplerate: int
     """The samplerate of the audio file in Hz."""
-    
+
     audio_channels: int
     """The number of audio channels."""
-    
+
     audio_dir: Path
     """The path of the audio file in temporary memory."""
 
     def __init__(
-        self, 
-        duration: float, 
-        samplerate: int, 
-        audio_channels: int, 
-        audio_dir: Path
+        self, duration: float, samplerate: int, audio_channels: int, audio_dir: Path
     ) -> None:
         """Initialize the AudioRecorder with the audio parameters."""
         self.duration = duration
@@ -38,26 +34,27 @@ class AccelRecorder(AudioRecorder):
         """Record an audio file using arecord."""
         now = datetime.datetime.now()
         audiofile_path = self.audio_dir / f'{now.strftime("%Y%m%d_%H%M%S")}.wav'
-        
+
         try:
             # Define the arecord command
             arecord_command = [
-                'arecord',
-                '-D', 'plughw:1,0',  # Replace with the correct device ID or name
-                '--format=S16_LE',
-                f'--channels={self.audio_channels}',
-                f'--rate={self.samplerate}',
-                f'--duration={int(self.duration)}',
-                str(audiofile_path)
+                "arecord",
+                "-D",
+                "plughw:1,0",  # Replace with the correct device ID or name
+                "--format=S16_LE",
+                f"--channels={self.audio_channels}",
+                f"--rate={self.samplerate}",
+                f"--duration={int(self.duration)}",
+                str(audiofile_path),
             ]
-            
+
             # Run the command
             print("START ACCEL - RECORDING - ARECORD")
             subprocess.run(arecord_command, check=True)
-        
+
         except subprocess.CalledProcessError as e:
-           print(f"Error during recording: {e}")
-           return None
+            print(f"Error during recording: {e}")
+            return None
 
         return data.Recording(
             path=audiofile_path,
