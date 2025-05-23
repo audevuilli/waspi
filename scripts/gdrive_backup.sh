faa11737ff7783d9178542c71c720a2a25ae357b
aude <aude.vuilli@gmail.com>
2025-05-23 14:38:11 +0100

    remove audio recording and sync from gdrive script

--- scripts/gdrive_backup.sh
+++ scripts/gdrive_backup.sh
@@ -3,27 +3,27 @@
 # rclone sync variables
 rcloneName="gdrive"
 syncRoot="${HOME}"
-DirImages="storages/images"
+#DirImages="storages/images"
 DirRecordingsAccel="storages/recordings/accel_rec"
-DirRecordingsMic="storages/recordings/audio_mic"
+#DirRecordingsMic="storages/recordings/audio_mic"
 remoteSyncDir="waspi_backup"
 
 # Get the date and folder_path of files to copy to gdrive
 date_now=$(date +'%Y_%m_%d')
 
 # Local Directories - RPi files paths to sync
-localSyncImages_Path="${syncRoot}/${DirImages}/${date_now}"
+#localSyncImages_Path="${syncRoot}/${DirImages}/${date_now}"
 localSyncRecAccel_Path="${syncRoot}/${DirRecordingsAccel}/"
-localSyncRecMic_Path="${syncRoot}/${DirRecordingsMic}/"
+#localSyncRecMic_Path="${syncRoot}/${DirRecordingsMic}/"
 #echo "$localSyncRecMic_Path"
 
 #Use rclone to copy folder over to gdrive
-rclone copy $localSyncImages_Path $rcloneName:$remoteSyncDir/$DirImages/$date_now
+#rclone copy $localSyncImages_Path $rcloneName:$remoteSyncDir/$DirImages/$date_now
 rclone copy $localSyncRecAccel_Path $rcloneName:$remoteSyncDir/$DirRecordingsAccel/$date_now
-rclone copy $localSyncRecMic_Path $rcloneName:$remoteSyncDir/$DirRecordingsMic/$date_now
+#rclone copy $localSyncRecMic_Path $rcloneName:$remoteSyncDir/$DirRecordingsMic/$date_now
 
 #Delete files saved on gdrive
 #rm -rf "$localSyncImages_Path"
-rm -rf "$DirImages"
+#rm -rf "$DirImages"
 rm "$DirRecordingsAccel/"*.wav
-rm "$DirRecordingsMic/"*.wav
+#rm "$DirRecordingsMic/"*.wav
