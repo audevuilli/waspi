61f032c49e40644b82fa1888e536833bb0d687f5
audevuilli <aude.vuilliomenet.18@ucl.ac.uk>
2025-06-01 16:32:10 +0100

    add program orchestrator to __init__

--- src/waspi/components/__init__.py
+++ src/waspi/components/__init__.py
@@ -1,9 +1,14 @@
 """Waspi components."""
+
 from waspi.components.accel_rec import AccelRecorder
 from waspi.components.audio import PyAudioRecorder
 from waspi.components.sensor_manager import SerialReceiver
-from waspi.components.message_factories import SensorValue_MessageBuilder, AccelLogger_MessageBuilder
+from waspi.components.message_factories import (
+    SensorValue_MessageBuilder,
+    AccelLogger_MessageBuilder,
+)
 from waspi.components.messengers import MQTTMessenger
+from waspi.components.program_orchestrator import ProgramOrchestrater
 from waspi.components.waspi_util import *
 from waspi.components.message_stores.sqlite import SqliteMessageStore
 from waspi.components.stores.sqlite import SqliteStore
@@ -11,10 +16,11 @@ from waspi.components.stores.sqlite import SqliteStore
 __all__ = [
     "AccelRecorder",
     "PyAudioRecorder",
-    "SerialReceiver", 
+    "SerialReceiver",
     "SensorValue_MessageBuilder",
     "AccelLogger_MessageBuilder",
     "MQTTMessenger",
+    "ProgramOrchestrater",
     "SqliteMessageStore",
     "SqliteStore",
 ]
