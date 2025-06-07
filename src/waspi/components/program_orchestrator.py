80afac376352dd1f209d8bf8196d857296235e3d
audevuilli <aude.vuilliomenet.18@ucl.ac.uk>
2025-06-07 15:21:26 +0100

    remove double lines in program orchestrator

--- src/waspi/components/program_orchestrator.py
+++ src/waspi/components/program_orchestrator.py
@@ -123,13 +123,10 @@ class ProgramOrchestrater:
             if response.status.value == 0:
                 logger.info(
                     f"MQTT message sent successfully in {phase_duration:.2f} seconds."
-                    f"MQTT message sent successfully in {phase_duration:.2f} seconds."
                 )
                 return True, phase_duration
             else:
                 logger.error(f"Failed to send MQTT message: {response.status.value}")
-                logger.error(f"Failed to send MQTT message {response}")
-                logger.error(f"Failed to send MQTT message {response}")
                 return False, phase_duration
 
         except Exception as e:
